cfa857b8b146abbf0a71ff48cd78f1ea5c638a21
sonanhson99 <sonanhson99@gmail.com>
2021-03-07 17:10:33 +0700

    sua loi

--- OOP/src/LuyenTap/plantFlowers.java
+++ OOP/src/LuyenTap/plantFlowers.java
@@ -1,20 +1,25 @@
 package LuyenTap;
 
 public class plantFlowers {
-    public static void main(String[] args) {
-        int dem = 0;
-        int a[] = {1, 0, 0, 0, 0, 0, 0, 1 ,0, 1, 0, 0};
-        int[] arr = new int[a.length+2];
-        arr[0] = arr[arr.length - 1] = 0;
-        for (int i = 0; i < a.length; i++) {
-            arr[i + 1] = a[i];
+    static boolean kt(int[] arr, int n) {
+        int[] a = new int[arr.length+2];
+        a[0] = a[a.length - 1] = 0;
+        for (int i = 0; i < arr.length; i++) {
+            a[i + 1] = arr[i];
         }
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i] == 0 && arr[i+1] == 0 && arr[i+2] == 0) {
-                arr[i + 1] = 1;
-                dem++;
+        for (int i = 1; i < a.length - 1; i++) {
+            if (a[i] == 0 && a[i+1] == 0 && a[i+2] == 0) {
+                a[i + 1] = 1;
+                n--;
             }
         }
-        System.out.println(dem);
+        if (n > 0) {
+            return false;
+        }
+        else return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 0, 0, 0, 0, 0, 1 ,0, 1, 0, 0};
+        System.out.println(kt(arr, 4));
     }
 }
